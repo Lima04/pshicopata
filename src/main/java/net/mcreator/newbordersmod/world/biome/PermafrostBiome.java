9884d9d5de05f271944d7e6173e0c2f58dece0bc
Lima04 <mufli@DESKTOP-4AEA9D6>
2020-12-21 10:50:17 -0300

    Adreno

---
+++ src/main/java/net/mcreator/newbordersmod/world/biome/PermafrostBiome.java
@@ -0,0 +1,56 @@
+
+package net.mcreator.newbordersmod.world.biome;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.common.BiomeManager;
+
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.IPlacementConfig;
+import net.minecraft.world.gen.feature.SeaGrassConfig;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.newbordersmod.NewBordersModModElements;
+
+@NewBordersModModElements.ModElement.Tag
+public class PermafrostBiome extends NewBordersModModElements.ModElement {
+	@ObjectHolder("new_borders_mod:permafrost")
+	public static final CustomBiome biome = null;
+	public PermafrostBiome(NewBordersModModElements instance) {
+		super(instance, 221);
+	}
+
+	@Override
+	public void initElements() {
+		elements.biomes.add(() -> new CustomBiome());
+	}
+
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+		BiomeManager.addSpawnBiome(biome);
+		BiomeManager.addBiome(BiomeManager.BiomeType.ICY, new BiomeManager.BiomeEntry(biome, 6));
+	}
+	static class CustomBiome extends Biome {
+		public CustomBiome() {
+			super(new Biome.Builder().downfall(0.3f).depth(2.5f).scale(0f).temperature(-0.1f).precipitation(Biome.RainType.SNOW)
+					.category(Biome.Category.ICY).waterColor(4159204).waterFogColor(329011).parent("snowy_tundra")
+					.surfaceBuilder(SurfaceBuilder.DEFAULT,
+							new SurfaceBuilderConfig(Blocks.SNOW.getDefaultState(), Blocks.ICE.getDefaultState(), Blocks.ICE.getDefaultState())));
+			setRegistryName("permafrost");
+			DefaultBiomeFeatures.addCarvers(this);
+			DefaultBiomeFeatures.addMonsterRooms(this);
+			DefaultBiomeFeatures.addStructures(this);
+			DefaultBiomeFeatures.addOres(this);
+			this.addStructure(Feature.IGLOO.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
+			this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.SEAGRASS.withConfiguration(new SeaGrassConfig(20, 0.3D))
+					.withPlacement(Placement.TOP_SOLID_HEIGHTMAP.configure(IPlacementConfig.NO_PLACEMENT_CONFIG)));
+		}
+	}
+}

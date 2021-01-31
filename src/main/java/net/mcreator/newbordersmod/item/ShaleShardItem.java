667afc9bf9db8672859b5836907ce0fb0bfc2815
ZitsyZits <andre@192.168.0.15>
2021-01-31 03:11:27 -0300

    why are you black

---
+++ src/main/java/net/mcreator/newbordersmod/item/ShaleShardItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.newbordersmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.newbordersmod.NewBordersModModElements;
+
+@NewBordersModModElements.ModElement.Tag
+public class ShaleShardItem extends NewBordersModModElements.ModElement {
+	@ObjectHolder("new_borders_mod:shale_shard")
+	public static final Item block = null;
+	public ShaleShardItem(NewBordersModModElements instance) {
+		super(instance, 537);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("shale_shard");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}

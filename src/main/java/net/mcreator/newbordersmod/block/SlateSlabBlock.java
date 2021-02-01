6308d0e2716493a2a1d46aba1b5fad6c0e1d7b1b
ZitsyZits <andre@192.168.0.15>
2021-02-01 12:32:29 -0300

    the singular update

---
+++ src/main/java/net/mcreator/newbordersmod/block/SlateSlabBlock.java
@@ -0,0 +1,53 @@
+
+package net.mcreator.newbordersmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.state.properties.SlabType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.newbordersmod.NewBordersModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@NewBordersModModElements.ModElement.Tag
+public class SlateSlabBlock extends NewBordersModModElements.ModElement {
+	@ObjectHolder("new_borders_mod:slate_slab")
+	public static final Block block = null;
+	public SlateSlabBlock(NewBordersModModElements instance) {
+		super(instance, 548);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends SlabBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1.1f, 3f).lightValue(0).harvestLevel(0)
+					.harvestTool(ToolType.PICKAXE));
+			setRegistryName("slate_slab");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, state.get(TYPE) == SlabType.DOUBLE ? 2 : 1));
+		}
+	}
+}

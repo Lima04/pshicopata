7662e24e583643cd1b79dd64b7fc03e572c1a92e
andremilanmatiello@gmail.com <andre@192.168.0.12>
2021-04-08 22:06:07 -0300

    the sun always shines on tv

---
+++ src/main/java/net/mcreator/newbordersmod/item/CakeFrostingItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.newbordersmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.newbordersmod.NewBordersModModElements;
+
+@NewBordersModModElements.ModElement.Tag
+public class CakeFrostingItem extends NewBordersModModElements.ModElement {
+	@ObjectHolder("new_borders_mod:cake_frosting")
+	public static final Item block = null;
+	public CakeFrostingItem(NewBordersModModElements instance) {
+		super(instance, 689);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1).rarity(Rarity.COMMON));
+			setRegistryName("cake_frosting");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
